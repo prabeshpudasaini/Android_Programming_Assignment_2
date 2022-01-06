b1bd99a77f545f1b560ed588d9eb8f7570db5fc3
Prabesh Pudasaini <72239377+prabeshpudasaini@users.noreply.github.com>
2022-01-06 20:01:14 +0545

    Changes

--- app/src/main/java/com/example/assignment_2/EnterPhoneActivity.java
+++ app/src/main/java/com/example/assignment_2/EnterPhoneActivity.java
@@ -1,15 +1,44 @@
 package com.example.assignment_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
 public class EnterPhoneActivity extends AppCompatActivity {
+
+    Button send;
+    EditText phone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enterphone);
+        setContentView(R.layout.activity_enter_phone);
+
+        send=findViewById(R.id.btnsend);
+        phone=findViewById(R.id.phone);
+
+        send.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String value = phone.getText().toString();
+                Intent i = new Intent(EnterPhoneActivity.this,MainActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("value",value);
+                i.putExtras(bundle);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button getPhone;
+    TextView phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getPhone=findViewById(R.id.btnphone);
+        phone=findViewById(R.id.tvphone);
 
         getPhone.setOnClickListener(new View.OnClickListener(){
 
@@ -27,5 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        Bundle bundle = getIntent().getExtras();
+        String value = bundle.getString("value");
+        phone.setText(value);
     }
 }
